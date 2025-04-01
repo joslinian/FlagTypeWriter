e80fa008b2cdc37b6fe0df705a421ece4cfac4fe
Ian Joslin <joslinian7@gmail.com>
2025-04-01 20:04:16 +0000

    Changes to be committed: 	new file:   .codesandbox/tasks.json 	modified:   client/src/App.tsx 	deleted:    client/src/pages/not-found.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,12 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
-      <Route component={NotFound} />
     </Switch>
   );
 }
@@ -18,7 +15,6 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router />
-      <Toaster />
     </QueryClientProvider>
   );
 }
